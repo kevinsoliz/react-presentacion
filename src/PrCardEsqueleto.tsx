f68f34c81811179bdb21d63b709c07a67e444d7e
Kevin Soliz <kevinsoliz10@gmail.com>
2026-02-08 11:53:28 +0100

    esquelto de la card producto

---
+++ src/PrCardEsqueleto.tsx
@@ -0,0 +1,14 @@
+
+const ProductoCard = () => {
+    return (
+        <div className="card d-inline-block m-2">
+            <div className="card-body">
+                <h5 className="card-title"> </h5>
+                <p className="card-text"> €</p>
+                <button className="btn btn-dark">Comprar</button>
+            </div>
+        </div>
+    );
+}
+
+export default ProductoCard;
